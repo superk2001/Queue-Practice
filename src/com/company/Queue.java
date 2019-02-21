40feebd84bf48ac62d49e7d0f0d12acc04e483d3
Kiernan <superk2001@users.noreply.github.com>
2019-02-20 20:20:01 -0800

    minor changes

--- src/com/company/Queue.java
+++ src/com/company/Queue.java
@@ -1,4 +1,18 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Queue {
+
+	public void enqueue(int i) {
+		// TODO Auto-generated method stub
+	}
+	
+	public int dequeue(){
+		return 0;	
+	}
+	
+	public ArrayList<Integer> toArrayList(){
+		return null;
+	}
 }
